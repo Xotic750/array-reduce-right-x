877831a35aa994af6dd9d06e3e906c98e8c2c0f6
Xotic750 <xotic750@gmail.com>
2017-08-09 21:10:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+<a href="https://travis-ci.org/Xotic750/array-reduce-right-x"
+   title="Travis status">
+<img
+   src="https://travis-ci.org/Xotic750/array-reduce-right-x.svg?branch=master"
+   alt="Travis status" height="18"/>
+</a>
+<a href="https://david-dm.org/Xotic750/array-reduce-right-x"
+   title="Dependency status">
+<img src="https://david-dm.org/Xotic750/array-reduce-right-x.svg"
+   alt="Dependency status" height="18"/>
+</a>
+<a href="https://david-dm.org/Xotic750/array-reduce-right-x#info=devDependencies"
+   title="devDependency status">
+<img src="https://david-dm.org/Xotic750/array-reduce-right-x/dev-status.svg"
+   alt="devDependency status" height="18"/>
+</a>
+<a href="https://badge.fury.io/js/array-reduce-right-x" title="npm version">
+<img src="https://badge.fury.io/js/array-reduce-right-x.svg"
+   alt="npm version" height="18"/>
+</a>
+<a name="module_array-reduce-right-x"></a>
+
+## array-reduce-right-x
+Reduce an array (from right to left) to a single value.
+
+**Version**: 1.0.0  
+**Author**: Xotic750 <Xotic750@gmail.com>  
+**License**: [MIT](&lt;https://opensource.org/licenses/MIT&gt;)  
+**Copyright**: Xotic750  
+<a name="exp_module_array-reduce-right-x--module.exports"></a>
+
+### `module.exports` ⇒ <code>\*</code> ⏏
+This method applies a function against an accumulator and each value of the
+array (from right-to-left) to reduce it to a single value..
+
+**Kind**: Exported member  
+**Returns**: <code>\*</code> - The value that results from the reduction.  
+**Throws**:
+
+- <code>TypeError</code> If array is null or undefined.
+- <code>TypeError</code> If callBack is not a function.
+- <code>TypeError</code> If called on an empty array without an initial value.
+
+
+| Param | Type | Description |
+| --- | --- | --- |
+| array | <code>array</code> | The array to iterate over. |
+| callBack | <code>function</code> | Function to execute for each element. |
+| [initialValue] | <code>\*</code> | Value to use as the first argument to the first  call of the callback. If no initial value is supplied, the first element in  the array will be used. Calling reduceRight on an empty array without an initial  value is an error. |
+
+**Example**  
+```js
+var reduceRight = require('array-reduceRight-right-x');
+
+var sum = reduceRight([0, 1, 2, 3], function (a, b) {
+  return a + b;
+}, 0);
+// sum is 6
+```
---
+++ tests/spec/test.js
@@ -0,0 +1,304 @@
+'use strict';
+
+var reduceRight;
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  if (typeof JSON === 'undefined') {
+    JSON = {};
+  }
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  var es7 = require('es7-shim');
+  Object.keys(es7).forEach(function (key) {
+    var obj = es7[key];
+    if (typeof obj.shim === 'function') {
+      obj.shim();
+    }
+  });
+  reduceRight = require('../../index.js');
+} else {
+  reduceRight = returnExports;
+}
+
+var createArrayLike = function (arr) {
+  var o = {};
+  arr.forEach(function (e, i) {
+    o[i] = e;
+  });
+
+  o.length = arr.length;
+  return o;
+};
+
+describe('reduceRight', function () {
+  var testSubject;
+
+  beforeEach(function () {
+    beforeEach(function () {
+      testSubject = [
+        1,
+        2,
+        3
+      ];
+    });
+  });
+
+  it('is a function', function () {
+    expect(typeof reduceRight).toBe('function');
+  });
+
+  it('should throw when array is null or undefined', function () {
+    expect(function () {
+      reduceRight();
+    }).toThrow();
+
+    expect(function () {
+      reduceRight(void 0);
+    }).toThrow();
+
+    expect(function () {
+      reduceRight(null);
+    }).toThrow();
+  });
+
+  describe('Array', function () {
+    it('should pass the correct arguments to the callback', function () {
+      var spy = jasmine.createSpy().andReturn(0);
+      reduceRight(testSubject, spy);
+      expect(spy.calls[0].args).toEqual([
+        3,
+        2,
+        1,
+        testSubject
+      ]);
+    });
+
+    describe('Array', function () {
+      it('should pass the correct arguments to the callback', function () {
+        var spy = jasmine.createSpy().andReturn(0);
+        reduceRight(testSubject, spy);
+        expect(spy.calls[0].args).toEqual([
+          3,
+          2,
+          1,
+          testSubject
+        ]);
+      });
+
+      it('should start with the right initialValue', function () {
+        var spy = jasmine.createSpy().andReturn(0);
+        reduceRight(testSubject, spy, 0);
+        expect(spy.calls[0].args).toEqual([
+          0,
+          3,
+          2,
+          testSubject
+        ]);
+      });
+
+      it('should not affect elements added to the array after it has begun', function () {
+        var arr = [
+          1,
+          2,
+          3
+        ];
+
+        var i = 0;
+        reduceRight(arr, function (a, b) {
+          i += 1;
+          if (i <= 4) {
+            arr.push(a + 3);
+          }
+
+          return b;
+        });
+
+        expect(arr).toEqual([
+          1,
+          2,
+          3,
+          6,
+          5
+        ]);
+
+        expect(i).toBe(2);
+      });
+
+      it('should work as expected for empty arrays', function () {
+        var spy = jasmine.createSpy();
+        expect(function () {
+          reduceRight([], spy);
+        }).toThrow();
+
+        expect(spy).not.toHaveBeenCalled();
+      });
+
+      it('should work as expected for empty arrays with an initial value', function () {
+        var spy = jasmine.createSpy();
+        var result;
+
+        result = reduceRight([], spy, '');
+        expect(spy).not.toHaveBeenCalled();
+        expect(result).toBe('');
+      });
+
+      it('should throw correctly if no callback is given', function () {
+        expect(function () {
+          reduceRight(testSubject);
+        }).toThrow();
+      });
+
+      it('should return the expected result', function () {
+        expect(reduceRight(testSubject, function (a, b) {
+          return String(a || '') + String(b || '');
+        })).toBe('321');
+      });
+
+      it('should not directly affect the passed array', function () {
+        var copy = testSubject.slice();
+        reduceRight(testSubject, function (a, b) {
+          return a + b;
+        });
+
+        expect(testSubject).toEqual(copy);
+      });
+
+      it('should skip non-set values', function () {
+        delete testSubject[1];
+        var visited = {};
+        reduceRight(testSubject, function (a, b) {
+          if (a) {
+            visited[a] = true;
+          }
+
+          if (b) {
+            visited[b] = true;
+          }
+
+          return 0;
+        });
+
+        expect(visited).toEqual({ 1: true, 3: true });
+      });
+    });
+
+    describe('Array-like objects', function () {
+      var testObject;
+
+      beforeEach(function () {
+        testObject = createArrayLike(testSubject);
+      });
+
+      it('should pass the correct arguments to the callback', function () {
+        var spy = jasmine.createSpy().andReturn(0);
+        reduceRight(testObject, spy);
+        expect(spy.calls[0].args).toEqual([
+          3,
+          2,
+          1,
+          testObject
+        ]);
+      });
+
+      it('should start with the right initialValue', function () {
+        var spy = jasmine.createSpy().andReturn(0);
+        reduceRight(testObject, spy, 0);
+        expect(spy.calls[0].args).toEqual([
+          0,
+          3,
+          2,
+          testObject
+        ]);
+      });
+
+      it('should not affect elements added to the array after it has begun', function () {
+        var arr = createArrayLike([
+          1,
+          2,
+          3
+        ]);
+
+        var i = 0;
+        reduceRight(arr, function (a, b) {
+          i += 1;
+          if (i <= 4) {
+            arr[i + 2] = a + 3;
+          }
+
+          return b;
+        });
+
+        expect(arr).toEqual({
+          0: 1,
+          1: 2,
+          2: 3,
+          3: 6,
+          4: 5,
+          length: 3 // does not get updated on property assignment
+        });
+
+        expect(i).toBe(2);
+      });
+
+      it('should work as expected for empty arrays', function () {
+        var spy = jasmine.createSpy();
+        expect(function () {
+          reduceRight({ length: 0 }, spy);
+        }).toThrow();
+
+        expect(spy).not.toHaveBeenCalled();
+      });
+
+      it('should throw correctly if no callback is given', function () {
+        expect(function () {
+          reduceRight(testObject);
+        }).toThrow();
+      });
+
+      it('should return the expected result', function () {
+        expect(reduceRight(testObject, function (a, b) {
+          return String(a || '') + String(b || '');
+        })).toBe('321');
+      });
+
+      it('should not directly affect the passed array', function () {
+        var copy = createArrayLike(testSubject);
+        reduceRight(testObject, function (a, b) {
+          return a + b;
+        });
+
+        expect(testObject).toEqual(copy);
+      });
+
+      it('should skip non-set values', function () {
+        delete testObject[1];
+        var visited = {};
+        reduceRight(testObject, function (a, b) {
+          if (a) {
+            visited[a] = true;
+          }
+
+          if (b) {
+            visited[b] = true;
+          }
+
+          return 0;
+        });
+
+        expect(visited).toEqual({ 1: true, 3: true });
+      });
+    });
+
+    it('should have a boxed object as list argument of callback', function () {
+      var actual;
+      // eslint-disable-next-line max-params
+      reduceRight('foo', function (accumulator, item, index, list) {
+        actual = list;
+      });
+
+      expect(typeof actual).toBe('object');
+      expect(Object.prototype.toString.call(actual)).toBe('[object String]');
+    });
+  });
+});
