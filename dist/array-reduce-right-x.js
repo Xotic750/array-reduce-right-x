e074276f607b64e22ef4a7c81fcbba202f9eb806
Graham Fairweather <xotic750@gmail.com>
2019-08-13 17:30:55 +0200

    Auto run

--- dist/array-reduce-right-x.js
+++ dist/array-reduce-right-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-13T11:49:19.422Z",
+  "date": "2019-08-13T15:30:48.759Z",
   "describe": "",
   "description": "Reduce an array (from right to left) to a single value.",
   "file": "array-reduce-right-x.js",
